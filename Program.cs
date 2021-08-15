7f8d6fa23ae3b16374ff155868aa74a3eaf127d2
Antoniocns <acnspeixe@gmail.com>
2021-08-14 21:54:19 -0300

    musica finalizada

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Pequena Música Noturna");
+            Console.WriteLine("---------------------------");
+            Console.ResetColor();
+            Console.WriteLine("Ouço a chuva batucando");
+            Console.WriteLine("Toda a noite no telhado");
+            Console.WriteLine("E os minutos escoando");
+            Console.WriteLine("Do presente pro passado");
+            Console.WriteLine("Uma, duas, quatro notas");
+            Console.WriteLine("Vai surgindo uma canção");
+            Console.WriteLine("Que me envolve e me conforta");
+            Console.WriteLine("Com lembranças de verão");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Numa delas mergulhei");
+            Console.WriteLine("Num amo de paixão");
+            Console.WriteLine("Se foi bom eu já nem sei");
+            Console.WriteLine("Hoje é só recordação");
+            Console.WriteLine("Essas coisas são do tempo");
+            Console.WriteLine("Não importa o que já fiz");
+            Console.WriteLine("Sinto o gosto do momento");
+            Console.WriteLine("Ouço a chuva e sou feliz");
+            Console.ResetColor();
+
+
+
+        }
+    }
+}
